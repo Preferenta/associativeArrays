2e441698705ee8d4947ca4c6d6c05d846a8d0ab5
Oksana D <preferenta@gmail.com>
2023-11-23 17:33:08 +0300

    array_values

--- arrFoo.php
+++ arrFoo.php
@@ -1,12 +1,19 @@
 <?php
-$data = ['first_name' => 'Mark', 'last_name' => 'Smith'];
-$keys = array_keys($data);
-foreach ($keys as $key) {
-    print_r ($data[$key]);
-}
+// $data = ['first_name' => 'Mark', 'last_name' => 'Smith'];
+// $keys = array_keys($data);
+// foreach ($keys as $key) {
+//     print_r ($data[$key]);
+// }
 
-$arr = ['macbook' => 'Pro', 'Iphon' => 'Pro14Max'];
-$keys = array_keys($arr);
-foreach ($keys as $key) {
-    print_r ($arr[$key]);
+// $arr = ['macbook' => 'Pro', 'Iphon' => 'Pro14Max'];
+// $keys = array_keys($arr);
+// foreach ($keys as $key) {
+//     print_r ($arr[$key]);
+// }
+
+// array_values
+$data = ['first_name' => 'Oksana', 'last_name' => 'De'];
+$values = array_values($data);
+foreach ($data as $key => $value) {
+    print_r ($value);
 }
