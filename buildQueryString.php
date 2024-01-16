2f63d20147d012873e13dae13b5cd738eaf29efd
Oksana D <preferenta@gmail.com>
2024-01-16 19:17:46 +0300

    ksort, foreach, implode

---
+++ buildQueryString.php
@@ -0,0 +1,11 @@
+<?php
+
+function buildQueryString($array)
+{
+    ksort($array);
+    $result = [];
+    foreach ($array as $key => $value) {
+        $result = "{$key}={$value}";
+    }
+    return implode("&", $result);
+}
