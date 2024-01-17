bc7a72e3ca9760b20f15bcf6adf58f77b88b8440
Oksana D <preferenta@gmail.com>
2024-01-17 13:57:46 +0300

    $result[]

--- buildQueryString.php
+++ buildQueryString.php
@@ -5,7 +5,7 @@ function buildQueryString($array)
     ksort($array);
     $result = [];
     foreach ($array as $key => $value) {
-        $result = "{$key}={$value}";
+        $result[] = "{$key}={$value}";
     }
     return implode("&", $result);
 }
